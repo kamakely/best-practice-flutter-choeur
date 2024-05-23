6cfa67f6e0dfced8d65e94c65d2d2bfe9340c6be
MOISE Rajesearison <moise.rajesearison@pulse.mg>
2024-05-23 06:23:18 +0300

    Ajout de l'interface de login

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+  static const Color brandGreen700 = Color(0xFF258A1E);
+  static const Color blackGrey = Color(0xFF434343);
+  static const Color black900 = Color(0xFF414141);
+  static const Color greenGradient = Color(0xFF2DB769);
+  static const Color blueGradient = Color(0xFF00A1E1);
+  static const Color brandGreen600 = Color(0xFF329B2A);
+  static const Color gray700 = Color(0xFF606060);
+  static const Color gray500 = Color(0xFF9D9D9D);
+  static const Color gray600 = Color(0xFF747474);
+  static const Color gray300 = Color(0xFFDFDFDF);
+  static const Color gray400 = Color(0xFFBCBCBC);
+  static const Color gray900 = Color(0xFF202020);
+  static const Color grey700 = Color(0xFF606060);
+  static const Color gray200 = Color(0xFFEDEDED);
+  static const Color gray100 = Color(0xFFF5F5F5);
+  static const Color gray50 = Color(0xFFFAFAFA);
+  static const Color blue = Color(0xFF00A1E1);
+  static const Color blue700 = Color(0xFF4E71A9);
+  static const Color green = Color(0xFF2DB769);
+  static const Color green50 = Color(0xFFECFDF3);
+  static const Color green700 = Color(0xFF027A48);
+  static const Color gree900 = Color(0xFFC5E4C2);
+  static const Color green600 = Color(0xFF039855);
+  static const Color brandBlue50 = Color(0xFFE6EBF2);
+  static const Color brandBlue400 = Color(0xFF4E71A9);
+  static const Color brandBlue900 = Color(0xFF002D64);
+  static const Color red500 = Color(0xFFF04438);
+  static const Color red600 = Color(0xFFD92D20);
+  static const Color red700 = Color(0xFFB42318);
+  static const Color red50 = Color(0xFFFEF3F2);
+  static const Color warning100 = Color(0xFFFEF0C7);
+  static const Color warning700 = Color(0xFFB54708);
+}
